e124b75d197cc733474a388ccc2f6d12402ffdc8
Viktorija Goferienė <v.goferiene@gmail.com>
2023-03-30 23:25:31 +0300

    Add 1 uzduotis

---
+++ Uzduotis_Nr_5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uzduotis_Nr_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //suma
+            Console.WriteLine(7 + 2);
+            //skirtumas
+            Console.WriteLine(7 - 2);
+            //sandauga
+            Console.WriteLine(7 * 2);
+            //dalyba
+            Console.WriteLine(7.0 / 2);
+        }
+    }
+}
